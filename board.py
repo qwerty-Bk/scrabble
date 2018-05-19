3ecccdf0f83cc4e4e3f71ff0434e8b2802cfaecb
katch <katch@katch.ru>
2018-05-19 18:05:12 +0700

    fun cntscr

--- board.py
+++ board.py
@@ -1,19 +1,30 @@
 # ВСЕ СЛОВА ГОРИЗОНТАЛЬНЫЕ СЛЕВА НАПРАВО
 
 class Board:
-    width = 15
-    height = 15
-
     def __init__(self, width=15, height=15):
         self.width = width
         self.height = height
+        self.wordmod = [
+            [1 for i in range(self.width)]
+            for j in range(self.height)
+        ]
+        self.letmod = [
+            [1 for i in range(self.width)]
+            for j in range(self.height)
+        ]
         self.cells = [
             ['*' for i in range(self.width)]
             for j in range(self.height)
         ]
 
-    def place(self, word, cop):
+    def place(self, word, row, column):
         for i in range(len(word)):
-            self.cells[cop[0]][cop[1] + i] = word[i]
+            self.cells[row][column + i] = word[i]
     
-    
+    def cntscr(self, word, row, column):
+        pts = 0
+        mult = 1
+        for i in range(len(word)):
+            mult *= self.wordmod[row][column + i]
+            pts += self.letmod[row][column + i] * Game.scores[word[i]]
+        return pts * mult
